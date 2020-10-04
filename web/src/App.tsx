7ec9d6d8f204e099f11c4ab69c0019533b6c20cf
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-10-04 14:52:49 -0700

    Initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./assets/images/styles/global.css";
+import Landing from "./pages/landing";
+
+function App() {
+  return (
+    <>
+      <Landing />
+    </>
+  );
+}
+
+export default App;
---
+++ web/src/pages/landing/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import "./styles.css";
+
+import logoImg from "../../assets/images/logo.svg";
+import landdingImg from "../../assets/images/landing.svg";
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+function Landing() {
+  return (
+    <>
+      <div id='page-landing'>
+        <div id='page-landing-content' className='container'>
+          <div className='logo-container'>
+            <img src={logoImg} alt='logoImg Proffy' />
+            <h2>Your online learning platform</h2>
+          </div>
+
+          <img src={landdingImg} alt='landdingImg' className='hero-image' />
+
+          <div className='buttons-container'>
+            <a href='\' className='study'>
+              <img src={studyIcon} alt='Study' />
+              Study
+            </a>
+
+            <a href='\' className='give-classes'>
+              <img src={giveClassesIcon} alt='giveClassesIcon' />
+              Study
+            </a>
+          </div>
+          <span className='total-connections'>
+            Total of 200 connections{" "}
+            <img src={purpleHeartIcon} alt='purpleHeartIcon' />{" "}
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Landing;
