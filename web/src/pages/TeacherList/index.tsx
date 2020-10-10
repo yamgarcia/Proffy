f9a400308784cc6f6bdc08bb1520f0f71732aeb9
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-10-10 15:01:42 -0700

    Teacher List style done

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 import PageHeader from "../../components/PageHeader";
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 const TeacherList: React.FC = () => {
   return (
@@ -22,6 +23,35 @@ const TeacherList: React.FC = () => {
             </div>
           </form>
         </PageHeader>
+
+        <main>
+          <article className='teacher-item'>
+            <header>
+              <img
+                src='https://avatars3.githubusercontent.com/u/46078084?s=460&u=b0ff170f13ccf75c5d168fea9f839188561fe461&v=4'
+                alt='Marcos Garcia'
+              />
+              <div>Marcos Garcia</div>
+              <span>Computer Architecture</span>
+            </header>
+
+            <p>
+              “Computer Architecture is the science of selecting hardware,
+              components to create computers that meet functional goals.”
+            </p>
+
+            <footer>
+              <p>
+                Price/hour
+                <strong>$80.90</strong>
+              </p>
+              <button type='button'>
+                <img src={whatsappIcon} alt='Contact-whatsapp' />
+                Get in Contact
+              </button>
+            </footer>
+          </article>
+        </main>
       </div>
     </>
   );
